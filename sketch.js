309a9fcc098aa7e9227107e5952e224cedc3a50b
kmarco4 <113069202+kmarco4@users.noreply.github.com>
2022-11-22 12:59:43 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
-function setup() {
-  createCanvas(400, 400); 
-  stroke ('orange');
-  v1 = createVector(width/2,height/2);
+//p5 reference code by sjnha
 
-}
+var r
+var b;
+function setup() { 
+  createCanvas(width, height);
+  r = color(200,0,0);
+  b = color(0,0,200);
+} 
 
-function draw() {
-  background(220);
-  fill('purple')
-  ellipse(v1.x, v1.y, mouseX, mouseY);
+function draw() { 
+  var t = map(mouseX,0,width,0,1.0);
+  var c = lerpColor(r,b,t);
+  background(c);
 }
